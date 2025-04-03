95ab692397fba3219206090a6c9cda634899221d
Denis Verepa <denisverepadev@gmail.com>
2025-04-03 11:02:09 +0300

    changes to Source model

--- LitExplorerDTO/SourceDTO.cs
+++ LitExplorerDTO/SourceDTO.cs
@@ -8,7 +8,7 @@
 
         public string HomePageUrl { get; set; } = null!;
 
-        public byte[]? Icon { get; set; }
+        public string? IconUrl { get; set; }
 
         public List<BookSourceDTO> BooksSources { get; set; } = null!;
     }
