18fab94c628fffdf24b960b43f1cb85fc4809e7f
Denis Verepa <denisverepadev@gmail.com>
2025-04-21 13:08:35 +0300

    Squashed commit of the following:
    commit 1b8315ed10ea2d5a2f67449c7ba6f6d833ea3cf8
    Author: Denis Verepa <denisverepadev@gmail.com>
    Date:   Wed Apr 16 12:34:53 2025 +0300
    
        added argument check
    
    commit c1db40f9cc99888f15daf3d35fb20afa7aea52f9
    Author: Denis Verepa <denisverepadev@gmail.com>
    Date:   Fri Apr 11 12:55:58 2025 +0300
    
        BCrypt implemented
    
    commit 1128d42783ad8249b3291e4906f11062b6776003
    Author: Denis Verepa <denisverepadev@gmail.com>
    Date:   Fri Apr 11 12:28:19 2025 +0300
    
        UserController implemented

---
+++ Controllers/UserController.cs
@@ -0,0 +1,81 @@
+﻿using LitExplorerAPI.LitExplorerDTO;
+using LitExplorerAPI.LitExplorerModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace LitExplorerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly LitExplorerContext litExplorerContext;
+
+        public UserController(LitExplorerContext litExplorerContext)
+            => this.litExplorerContext = litExplorerContext;
+
+        [HttpPost("signUp")]
+        public async Task<IActionResult> SignUp([FromBody] UserDTO userDTO)
+        {
+            try
+            {
+                if (userDTO == null)
+                    return BadRequest("Received user was null!");
+
+                if(userDTO.Email.IsNullOrEmpty() || userDTO.Password.IsNullOrEmpty())
+                    return BadRequest("Received user data was empty!");
+
+                var userDb = await litExplorerContext.Users.FirstOrDefaultAsync(u => u.Email == userDTO.Email);
+                if(userDb == null)
+                {
+                    string hashedPassword = BCrypt.Net.BCrypt.HashPassword(userDTO.Password, workFactor:12);
+                    userDb = new User() { Email = userDTO.Email, HashedPassword = hashedPassword };
+                    
+                    await litExplorerContext.Users.AddAsync(userDb);
+                    await litExplorerContext.SaveChangesAsync();
+
+                    userDTO.UserId = userDb.UserId;
+                    userDTO.RegistrationDate = userDb.RegistrationDate;
+
+                    return Ok(userDTO);
+                }
+
+                return Conflict("Such user is already registered!");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { Message = "An error occurred while signing up", Error = ex.Message });
+            }
+        }
+
+        [HttpPost("signIn")]
+        public async Task<IActionResult> SignIn([FromBody] UserDTO userDTO)
+        {
+            try
+            {
+                if (userDTO == null)
+                    return BadRequest("Received user was null!");
+
+                if (userDTO.Email.IsNullOrEmpty() || userDTO.Password.IsNullOrEmpty())
+                    return BadRequest("Received user data was empty!");
+
+                var userDb = await litExplorerContext.Users.FirstOrDefaultAsync(u => u.Email == userDTO.Email);
+                if (userDb != null)
+                {
+                    userDTO.UserId = userDb.UserId;
+                    userDTO.RegistrationDate = userDb.RegistrationDate;
+
+                    return BCrypt.Net.BCrypt.Verify(userDTO.Password, userDb.HashedPassword) ? Ok(userDTO) : BadRequest("Wrong password!");
+                }
+
+                return Conflict("Acount under provided email address doesn't exist!");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { Message = "An error occurred while signing in", Error = ex.Message });
+            }
+        }
+    }
+}
