30774ee36efd675fdc074e228d318aa7c55bfd9b
Denis Verepa <denisverepadev@gmail.com>
2025-05-14 07:53:47 +0300

    update to modern Library model

---
+++ LitExplorerModels/ReadingHistory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LitExplorerAPI.LitExplorerModels;
+
+public partial class ReadingHistory
+{
+    public int UserId { get; set; }
+
+    public int BookSourceId { get; set; }
+
+    public int? LastReadChapter { get; set; }
+
+    public DateTime? LastReadingUpdateDate { get; set; }
+
+    public virtual BooksSource BookSource { get; set; } = null!;
+
+    public virtual User User { get; set; } = null!;
+}
