d14297c304c7ddf764393411841d96a024198cd3
Denis Verepa <denisverepadev@gmail.com>
2025-06-01 12:21:44 +0300

    lib fixes

---
+++ LitExplorerDTO/LibraryStatusOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace LitExplorerAPI.LitExplorerDTO
+{
+    public enum LibraryStatusOptions
+    {
+        Reading = 1,
+        Favorite = 2,
+        Planning = 3,
+        Finished = 4,
+        Dropped = 5
+    }
+}
