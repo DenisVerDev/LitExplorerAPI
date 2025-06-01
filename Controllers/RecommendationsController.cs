447509cdbd8e1debdb0ca0bd321064b588816217
Denis Verepa <denisverepadev@gmail.com>
2025-06-01 19:16:10 +0300

    personal recommendations

--- Controllers/RecommendationsController.cs
+++ Controllers/RecommendationsController.cs
@@ -21,6 +21,9 @@ namespace LitExplorerAPI.Controllers
         {
             try
             {
+                if (rOptions == RecommendationsOptions.Personal)
+                    return await GetPersonalRecommendations(userDTO, count);
+
                 var userId = userDTO == null ? 0 : userDTO.UserId;
 
                 // 1st part - filtering candidates
@@ -44,10 +47,6 @@ namespace LitExplorerAPI.Controllers
 
                     case RecommendationsOptions.Hottest:
                         query = GetHottest(query);
-                        break;
-
-                    case RecommendationsOptions.Personal:
-                        query = GetPersonal(query, userDTO);
                         break;
 
                     default: throw new Exception("There is no such supported recommendation option!");
@@ -85,6 +84,140 @@ namespace LitExplorerAPI.Controllers
             }
         }
 
+        private async Task<IActionResult> GetPersonalRecommendations([FromBody] UserDTO? userDTO, int count)
+        {
+            try
+            {
+                if (userDTO == null || userDTO.UserId == 0)
+                    throw new Exception("Received user's data was null");
+
+                int userId = userDTO.UserId;
+                // 1. Отримуємо всі BookId, які користувач читав або додав у бібліотеку
+                var userBookIds = await litExplorerContext.Libraries
+                                       .Where(l => l.UserId == userId)
+                                       .Select(l => l.BookId)
+                                       .ToListAsync();
+                // Додаємо книги з історії читання (BookSourceId -> BookId)
+                var historyBookIds = await litExplorerContext.ReadingHistories
+                                       .Where(rh => rh.UserId == userId)
+                                       .Select(rh => rh.BookSource.BookId)
+                                       .ToListAsync();
+                userBookIds.AddRange(historyBookIds);
+                userBookIds = userBookIds.Distinct().ToList();
+
+                if (userBookIds.Count == 0)
+                    throw new Exception("User doesn't have any previous interactions with books");
+
+                // 2. Вибираємо вектори ознак для книг користувача з кешу BooksFeatures
+                var userFeaturesList = await litExplorerContext.BooksFeatures
+                                            .Where(bf => userBookIds.Contains(bf.BookId))
+                                            .ToListAsync();
+                // Будуємо середній профіль користувача (усереднений вектор)
+                float[] userProfileVector = new float[0];
+                int vectorLength = 0;
+                if (userFeaturesList.Count > 0)
+                {
+                    // Ініціалізуємо профіль нульовим вектором потрібної довжини
+                    vectorLength = userFeaturesList[0].VectorBlob.Length / sizeof(float);
+                    userProfileVector = new float[vectorLength];
+                }
+                foreach (var bf in userFeaturesList)
+                {
+                    // Десеріалізуємо вектор книги з байтів
+                    int len = bf.VectorBlob.Length / sizeof(float);
+                    float[] bookVector = new float[len];
+                    Buffer.BlockCopy(bf.VectorBlob, 0, bookVector, 0, bf.VectorBlob.Length);
+                    // Додаємо до профілю (поелементно)
+                    for (int i = 0; i < len; i++)
+                    {
+                        userProfileVector[i] += bookVector[i];
+                    }
+                }
+                // Усереднюємо суму ознак профілю
+                for (int i = 0; i < userProfileVector.Length; i++)
+                {
+                    userProfileVector[i] /= userFeaturesList.Count;
+                }
+
+                // 3. Обчислюємо косинусну схожість профілю з усіма іншими книгами:contentReference[oaicite:11]{index=11}
+                var allFeatures = await litExplorerContext.BooksFeatures
+                                     .Where(bf => !userBookIds.Contains(bf.BookId)).Take(500)
+                                     .ToListAsync();
+                // Змінні для топ-рекомендацій
+                var topRecommendations = new List<(int BookId, double CosineSimilarity)>();
+
+                // Попередньо обчислюємо норму профілю для косинусного подібності
+                double profileNorm = 0.0;
+                for (int i = 0; i < userProfileVector.Length; i++)
+                    profileNorm += userProfileVector[i] * userProfileVector[i];
+                profileNorm = Math.Sqrt(profileNorm);
+
+                foreach (var bf in allFeatures)
+                {
+                    // Десеріалізуємо вектор книги
+                    int len = bf.VectorBlob.Length / sizeof(float);
+                    float[] bookVector = new float[len];
+                    Buffer.BlockCopy(bf.VectorBlob, 0, bookVector, 0, bf.VectorBlob.Length);
+
+                    // Обчислюємо косинус(userProfile, bookVector) = (A·B) / (||A||*||B||)
+                    double dot = 0.0;
+                    double bookNorm = 0.0;
+                    for (int i = 0; i < len; i++)
+                    {
+                        dot += userProfileVector[i] * bookVector[i];
+                        bookNorm += bookVector[i] * bookVector[i];
+                    }
+                    if (bookNorm == 0 || profileNorm == 0)
+                    {
+                        continue; // пропускаємо книги без даних або порожній профіль
+                    }
+                    bookNorm = Math.Sqrt(bookNorm);
+                    double cosine = dot / (profileNorm * bookNorm);
+                    topRecommendations.Add((bf.BookId, cosine));
+                }
+
+                // 4. Сортуємо книги за спаданням схожості і беремо топ-N
+                var recommendedIds = topRecommendations
+                                        .OrderByDescending(x => x.CosineSimilarity)
+                                        .Take(count)    // кількість рекомендацій
+                                        .Select(x => x.BookId)
+                                        .ToList();
+
+                if (recommendedIds.Count == 0)
+                    throw new Exception("Can't find suitable recommendations");
+
+                // 5. Завантажуємо метадані для рекомендованих книг і формуємо відповідь
+                var recommendedMeta = await litExplorerContext.BooksMeta
+                    .Include(bm => bm.BookSource)
+                        .ThenInclude(bs => bs.Book)
+                            .ThenInclude(b => b.Libraries)
+                                .ThenInclude(lib => lib.Status)
+                    .Include(bm => bm.BookSource)
+                        .ThenInclude(bs => bs.Tags)
+                    .Include(bm => bm.BookSource)
+                        .ThenInclude(bs => bs.ReadingHistories)
+                    .Include(bm => bm.Author)
+                    .Where(bm => recommendedIds.Contains(bm.BookSource.BookId))
+                    .ToListAsync();
+
+                // Розміщуємо BooksMeta у порядку рекомендованих BookId
+                var sortedBooksMeta = recommendedIds
+                    .SelectMany(id => recommendedMeta.Where(bm => bm.BookSource.BookId == id))
+                    .ToList();
+
+                // Трансформуємо в DTO для відповіді
+                var booksDTO = ToBookDTO(sortedBooksMeta);
+                var authorsDTO = ToAuthorDTO(sortedBooksMeta);
+                var result = new { Books = booksDTO, Authors = authorsDTO };
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                //return StatusCode(500, new { Message = "An error occurred while recommending books", Error = ex.Message });
+                return await RecommendBooks(userDTO, RecommendationsOptions.BestOfMonth, count);
+            }
+        }
+
         private IQueryable<BooksMetum> GetBestOfMonth(IQueryable<BooksMetum> query)
         {
             var now = DateTime.UtcNow;
@@ -105,11 +238,6 @@ namespace LitExplorerAPI.Controllers
                          .OrderByDescending(bm => bm.FirstChapterReleaseDate);
 
             return query;
-        }
-
-        private IQueryable<BooksMetum> GetPersonal(IQueryable<BooksMetum> query, UserDTO? userDTO)
-        {
-            return query; // stand in code
         }
 
         private List<BookDTO>? ToBookDTO(List<BooksMetum> booksMeta)
---
+++ LitExplorerModels/BooksFeature.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LitExplorerAPI.LitExplorerModels;
+
+public partial class BooksFeature
+{
+    public int BookId { get; set; }
+
+    public byte[] VectorBlob { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public virtual Book Book { get; set; } = null!;
+}
